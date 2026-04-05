365a260a248d34fb14cbe46e2fe40649535976c4
jagrest2 <joshuaagrest@gmail.com>
2026-04-05 10:44:52 -0500

    V6

--- app/simulation.py
+++ app/simulation.py
@@ -20,19 +20,19 @@ df_poss.columns = df_poss.columns.str.lower()
 
 def simulate_possession(offense_team, defense_team):
     # 1. Did a turnover happen?
-    to_prob = (df_tov.loc[df_tov['Team'] == offense_team, 'tov%'].iloc[0] + 
-               df_opp.loc[df_opp['Team'] == defense_team, 'tov%'].iloc[0]) / 2
+    to_prob = (df_tov.loc[df_tov['team'] == offense_team, 'tov%'].iloc[0] + 
+               df_opp.loc[df_opp['team'] == defense_team, 'tov%'].iloc[0]) / 2
                
     if np.random.random() < to_prob:
         return 0, "Turnover"
 
     # 2. If no turnover, they take a shot. Did it go in?
-    efg = df_efg.loc[df_efg['Team'] == offense_team, 'efg%'].iloc[0]
+    efg = df_efg.loc[df_efg['team'] == offense_team, 'efg%'].iloc[0]
     if np.random.random() < efg:
         return 2, "Made 2pt Basket" # Simplified for now
     
     # 3. If they missed, did they get the offensive board?
-    or_rate = df_orb.loc[df_orb['Team'] == offense_team, 'orb%'].iloc[0]
+    or_rate = df_orb.loc[df_orb['team'] == offense_team, 'orb%'].iloc[0]
     if np.random.random() < or_rate:
         points, desc = simulate_possession(offense_team, defense_team) # Recursive reset
         return points, f"Missed Shot -> Off Rebound -> {desc}"
